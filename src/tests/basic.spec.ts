85d4092ef5ab34a8ffccb1fefafc5948a9bb0274
ImOverlord <thibault-alexandre.faure@epitech.eu>
2019-03-14 08:16:15 +0100

    Improved Documentation, Added getByKey and getByValue

--- src/tests/basic.spec.ts
+++ src/tests/basic.spec.ts
@@ -4,81 +4,126 @@ import { DependencyB } from './mocks/dependencyB.mock';
 import { DependencyC } from './mocks/dependencyC.mock';
 import { DependencyD } from './mocks/dependencyD.mock';
 import { DependencyE } from './mocks/dependencyE.mock';
+import { DependencyF } from './mocks/dependencyF.mock';
 
 describe("Basic Injection Test", () => {
 
     let injector: Injector;
 
-    beforeEach(() => {
-        injector = new Injector();
+    describe("Register", () => {
+
+        beforeEach(() => {
+            injector = new Injector();
+        });
+
+        it("Should add a dependencie", () => {
+            const dep: DependencyA = injector.register("DependencyA", DependencyA);
+
+            expect(injector['container'].length).toBe(1);
+            expect(dep).toBeInstanceOf(DependencyA);
+        });
+
+        it("Should Add 2 times the same dependency", () => {
+            injector.register('DependencyA', DependencyA);
+            injector.register('DependencyA', DependencyA);
+
+            expect(injector['container'].length).toBe(1);
+        });
+
+        it("Should register complex dependency", () => {
+            const dep: DependencyD = injector.register("DependencyD", DependencyD);
+
+            expect(injector['container'].length).toBe(2);
+            expect(dep).toBeInstanceOf(DependencyD);
+        });
+
     });
 
-    it("Should add a dependencie", () => {
-        const dep: DependencyA = injector.register("DependencyA", DependencyA);
+    describe("Inject", () => {
 
-        expect(injector['container'].length).toBe(1);
-        expect(dep).toBeInstanceOf(DependencyA);
+        beforeEach(() => {
+            injector = new Injector();
+        });
+
+        it("Should retreive", () => {
+            const dep: DependencyC = injector.inject(DependencyC);
+
+            expect(dep).not.toBeNull();
+            expect(dep).not.toBeUndefined();
+            expect(dep.a).not.toBeNull();
+            expect(dep.a).not.toBeUndefined();
+        });
+
+        it("Shouldn't instanciated multiple time the same dependency", () => {
+            const dep: DependencyC = injector.inject(DependencyC);
+            const dep2: DependencyD = injector.inject(DependencyD);
+
+            expect(dep.a.data).toBe(0);
+            dep.update();
+            expect(dep.a.data).toBe(1);
+            expect(dep2.a.data).toBe(1);
+        });
+
+        it("Should register Mocks", () => {
+            injector.register("DependencyA", DependencyB);
+
+            const dep: DependencyA = injector.inject(DependencyA);
+            expect(injector['container'].length).toBe(1);
+            expect(dep).toBeInstanceOf(DependencyB);
+        });
+
+        it("Should resolve a 'complex' dependency tree", () => {
+            const dep: DependencyE = injector.inject(DependencyE);
+
+            expect(dep).not.toBeNull();
+            expect(dep).not.toBeUndefined();
+            expect(dep.a).not.toBeNull();
+            expect(dep.a).not.toBeUndefined();
+            expect(dep.d).not.toBeNull();
+            expect(dep.d).not.toBeUndefined();
+            expect(dep.d.a).not.toBeNull();
+            expect(dep.d.a).not.toBeUndefined();
+            expect(dep.a.data).toBe(0);
+            dep.update();
+            expect(dep.a.data).toBe(1);
+            expect(dep.d.a.data).toBe(1);
+        });
+
     });
 
-    it("Should Add 2 times the same dependency", () => {
-        injector.register('DependencyA', DependencyA);
-        injector.register('DependencyA', DependencyA);
+    describe("getByKey", () => {
 
-        expect(injector['container'].length).toBe(1);
+        beforeEach(() => {
+            injector = new Injector();
+        });
+
+        it("Should retreive class with a certain key as metadata", () => {
+            injector.register("DependencyF", DependencyF);
+            injector.register("DependencyB", DependencyA);
+
+            const arr = injector.getByKey('type');
+            expect(arr.length).toBe(1);
+            expect(arr[0]).toBeInstanceOf(DependencyF);
+        });
+
     });
 
-    it("Should register complex dependency", () => {
-        const dep: DependencyD = injector.register("DependencyD", DependencyD);
+    describe("getByValue", () => {
 
-        expect(injector['container'].length).toBe(2);
-        expect(dep).toBeInstanceOf(DependencyD);
+        beforeEach(() => {
+            injector = new Injector();
+        });
+
+        it("Should retreive class with a certain key as metadata", () => {
+            injector.register("DependencyF", DependencyF);
+            injector.register("DependencyF", DependencyB);
+            injector.register("DependencyA", DependencyA);
+
+            const arr = injector.getByValue('type', "plugin");
+            expect(arr.length).toBe(1);
+            expect(arr[0]).toBeInstanceOf(DependencyF);
+        });
+
     });
 
-    it("Should retreive", () => {
-        const dep: DependencyC = injector.inject(DependencyC);
-
-        expect(dep).not.toBeNull();
-        expect(dep).not.toBeUndefined();
-        expect(dep.a).not.toBeNull();
-        expect(dep.a).not.toBeUndefined();
-    });
-
-    it("Shouldn't instanciated multiple time the same dependency", () => {
-        const dep: DependencyC = injector.inject(DependencyC);
-        const dep2: DependencyD = injector.inject(DependencyD);
-
-        expect(dep.a.data).toBe(0);
-        dep.update();
-        expect(dep.a.data).toBe(1);
-        expect(dep2.a.data).toBe(1);
-    });
-
-    it("Should register Mocks", () => {
-        injector.register("DependencyA", DependencyB);
-
-        const dep: DependencyA = injector.inject(DependencyA);
-        expect(injector['container'].length).toBe(1);
-        expect(dep).toBeInstanceOf(DependencyB);
-    });
-
-    it("Should resolve a 'complex' dependency tree", () => {
-        const dep: DependencyE = injector.inject(DependencyE);
-
-        expect(dep).not.toBeNull();
-        expect(dep).not.toBeUndefined();
-        expect(dep.a).not.toBeNull();
-        expect(dep.a).not.toBeUndefined();
-        expect(dep.d).not.toBeNull();
-        expect(dep.d).not.toBeUndefined();
-        expect(dep.d.a).not.toBeNull();
-        expect(dep.d.a).not.toBeUndefined();
-        expect(dep.a.data).toBe(0);
-        dep.update();
-        expect(dep.a.data).toBe(1);
-        expect(dep.d.a.data).toBe(1);
-    });
-
-    afterEach(() => {
-        injector = null;
-    });
 });
